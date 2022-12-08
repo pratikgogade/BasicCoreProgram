adf3b3f53e24c0001d0d4b96588913678a0055ea
unknown <Pratik@Example.com>
2022-12-08 15:36:48 +0530

    [Pratik]Add:Swapping Numbers

---
+++ BasicCoreProgram/SwapNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class SwapNumbers
+    {
+        public void Swap()
+        {
+            Console.WriteLine("Swap Two Numbers");
+            Console.WriteLine("Enter The Numbers For Swapping");
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            Console.WriteLine("Before Swapping Numbers " + first + " , " + second);
+            int temp = first;
+            first = second;
+            second = temp;
+            Console.WriteLine("After Swapping The Numbers Are " + first + " & " + second);
+        }
+    }
+}
