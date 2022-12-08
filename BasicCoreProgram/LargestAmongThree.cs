3485c9554729ab65f79ff257e7800b1dadcf51e4
unknown <Pratik@Example.com>
2022-12-08 15:51:10 +0530

    [Pratik]Add:Largest Among Three Numbers

---
+++ BasicCoreProgram/LargestAmongThree.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class LargestAmongThree
+    {
+        public void Largest()
+        {
+            Console.WriteLine("Find the Largest Number Among The Three");
+            Console.WriteLine("Enter The Numbers");
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+            if (first >= second && first >= third)
+            {
+                Console.WriteLine(first + " Is The Largest Among The Three Numbers");
+            }
+            else if (second >= third)
+            {
+                Console.WriteLine(second + " Is The Largest Among The Three Numbers");
+            }
+            else
+            {
+                Console.WriteLine(third + " Is The Largest Among The Three Numbers");
+            }
+        }
+    }
+}
--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -11,7 +11,7 @@ namespace BasicCoreProgram
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("1.FlipCoin" + "\n" + "2.Leap Year" + "\n" + "3.Power Of Two" + "\n" + "4.Harmonic Number" + "\n" + "5.Prime Factor" + "\n" + "6.Quotient And Remainder" + "\n" + "7.Swapping Numbers" + "\n" + "8.Even And Odd" + "\n" + "9.Vowel Or Consonant" + "\n" + "10.Exit");
+                Console.WriteLine("1.FlipCoin" + "\n" + "2.Leap Year" + "\n" + "3.Power Of Two" + "\n" + "4.Harmonic Number" + "\n" + "5.Prime Factor" + "\n" + "6.Quotient And Remainder" + "\n" + "7.Swapping Numbers" + "\n" + "8.Even And Odd" + "\n" + "9.Vowel Or Consonant" + "\n" + "10.Largest Number" + "\n" + "11.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -50,6 +50,10 @@ namespace BasicCoreProgram
                     case 9:
                         VowelConsonant vowelConsonant = new VowelConsonant();
                         vowelConsonant.Alphabet();
+                        break;
+                    case 10:
+                        LargestAmongThree largestAmongThree = new LargestAmongThree();
+                        largestAmongThree.Largest();
                         break;
                     default:
                         break;
