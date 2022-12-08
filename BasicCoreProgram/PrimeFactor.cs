12d1e5926fb542ba57f35a344a092d1c0f211e42
unknown <Pratik@Example.com>
2022-12-08 15:25:16 +0530

    [Pratik]Add:Power Of Two

---
+++ BasicCoreProgram/PrimeFactor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class PrimeFactor
+    {
+        public void Factor()
+        {
+            Console.WriteLine("Compute The Prime Factorization Of N");
+            int result = 1;
+            Console.WriteLine("Enter Number");
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                result = result * i;
+            }
+            Console.WriteLine("Factorial of " + num + " is = " + result);
+        }
+    }
+}
--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -11,7 +11,7 @@ namespace BasicCoreProgram
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("1.FlipCoin" + "\n" + "2.Leap Year" + "\n" + "3.Power Of Two" + "\n" + "4.Harmonic Number" + "\n" + "5.Exit");
+                Console.WriteLine("1.FlipCoin" + "\n" + "2.Leap Year" + "\n" + "3.Power Of Two" + "\n" + "4.Harmonic Number" + "\n" + "5.Prime Factor" + "\n" + "6.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -30,6 +30,10 @@ namespace BasicCoreProgram
                     case 4:
                         HarmonicNumber harmonicNumber = new HarmonicNumber();
                         harmonicNumber.Harmonic();
+                        break;
+                    case 5:
+                        PrimeFactor primeFactor = new PrimeFactor();
+                        primeFactor.Factor();
                         break;
                     default:
                         break;
